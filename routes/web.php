91c3c9df9344c30f102a256e99bfd95771bf2d06
LutfiZkn <lutfizaki2256@gmail.com>
2026-07-30 14:19:05 +0800

    membuat controller untuk kecamatan dan kelurahan serta menambahkan routesnya di web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RumahController;
+use App\Http\Controllers\KecamatanController;
+use App\Http\Controllers\KelurahanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('rumah', RumahController::class);
+Route::resource('kecamatan', KecamatanController::class);
+Route::resource('kelurahan', KelurahanController::class);
